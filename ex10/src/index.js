f3d8cc1584faa0fe071dfa60cd93e009a249cb39
Martin337 <gamarayburst@gmail.com>
2021-09-16 21:45:38 +0200

    Sto posto

---
+++ ex10/src/index.js
@@ -0,0 +1,17 @@
+function main(){
+    // only change code below this line
+
+        var myFirstName = 'Martin';
+        var myLastName = 'Galić';
+        var myHomeTown = 'Mostar';
+
+    // only change code below this line
+    return{
+        myFirstName,
+        myLastName,
+        myHomeTown
+    };
+}
+
+console.log(main());
+module.exports = main;
